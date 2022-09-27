2fd9ebe9559900520a9d0dc0f2133ead250f609c
suraj.neupane <suraj.neupane151@gmail.com>
2022-09-27 14:53:11 +0545

    add: default colors, add: error handler, refactor: axios get beers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Routes from "./Routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+
 
 function App() {
   return (
-    <Routes />
+    <SSRProvider>
+      <Routes />
+    </SSRProvider>
   );
 }
 
--- src/Pages/Home/view.jsx
+++ src/Pages/Home/view.jsx
@@ -1,57 +1,91 @@
-import React, { useState, useRef, useCallback, useEffect, forwardRef} from 'react';
-// import {} from 'react-bootstrap';
-
+import React, { useState, useRef, useCallback, useEffect, forwardRef } from 'react';
 import axios from 'axios';
 import Item from "Components/Items";
 
+import Placeholder from 'react-bootstrap/Placeholder';
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 const Homapage = (props) => {
-    const [page, setPage] = useState(1);
-    const [beers, setBeers] = useState([])
-    const [loading, setLoading] = useState(false)
-    
-    
-    async function getBeers() {
-        setLoading(true)
-        
-        const { data } = await axios.get('https://api.punkapi.com/v2/beers', {
-            params: {
-                per_page: 3,
-                page
-            }
-        });
-
-        console.log({page});
-        
-        setLoading(false);
-        setBeers(current => [...current, ...data]);
-        setPage(prev=>prev +  1);
+    const defaultError = {
+        error: false,
+        code: 'UNKNOWN',
+        message: 'Unknown Error',
     }
-    
-    useEffect(() => {
-        getBeers();
-        console.log({beers});
+    const defaultpage = 1;
+    const defaultItems = 3;
 
-        //remove all beers when unmounted
-        return ()=>{
-            setBeers([]);
-            setPage(1);
-        };
-    }, [])
     
+    const [page, setPage] = useState(defaultpage);
+    const [beers, setBeers] = useState([])
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(defaultError)
+
+
+    async function getBeers() {
+        setPage(prev => prev + 1);
+        setLoading(true);
+        try {
+            const beer_api = await axios.get('https://api.punkapi.com/v2/beers', {
+                params: {
+                    per_page: defaultItems,
+                    page
+                }
+            });
+
+            //throw error even if error sent as response.
+            if(beer_api?.error) throw new Error(beer_api.error);
+
+            //extract data and reset loader
+            const data = Array.isArray(beer_api?.data) ? beer_api.data : [];
+            setLoading(false);
+            
+            // add beers to list
+            setBeers(current => [...current, ...data]);
+        } catch (e) {
+            setError({
+                ...defaultError,
+                error: true,
+                code: e.code,
+                message: e.message
+            });
+        }
+    }
+
+    // function loadMore() {
+    //     setPage(prev => prev + 1);
+    // }
+
+    // useEffect(() => {
+    //     getBeers();
+
+    //     // console.log({page}, {beers});
+    //     //remove all beers when unmounted
+    //     return () => {
+    //         setBeers([]);
+    //         setPage(defaultpage);
+    //         setError(defaultError);
+    //         setLoading(false);
+    //     };
+    // }, []);
+
     return (
         <section >
+            {error.error &&
+                <Alert variant="danger" onClose={() => setError(defaultError)} dismissible>
+                    <Alert.Heading>{error.code}</Alert.Heading>
+                    <p>{error.message}</p>
+                </Alert>}
+
             {!!beers.length && (
                 <div>
                     {beers.map((item_data, index) => (
-                        <Item item_data = {item_data}/>
+                        <Item item_data={item_data} />
                     ))}
                 </div>
             )}
+            <Button onClick={getBeers}>Load More</Button>
 
-            <Button>Load More</Button>
-            
             {loading && (
                 <>Loading...</>
             )}
