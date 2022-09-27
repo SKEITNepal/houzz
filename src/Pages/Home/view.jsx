fea54746c7d2a1374e6c6f7b8b21e87800d98306
suraj.neupane <suraj.neupane151@gmail.com>
2022-09-27 13:53:10 +0545

    add: - home page beer list draft - API using axios -page increamental  -bootstrap button

--- src/Pages/Home/view.jsx
+++ src/Pages/Home/view.jsx
@@ -1,11 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback, useEffect, forwardRef} from 'react';
+// import {} from 'react-bootstrap';
+
+import axios from 'axios';
+import Item from "Components/Items";
+
+import Button from "react-bootstrap/Button";
 
 const Homapage = (props) => {
+    const [page, setPage] = useState(1);
+    const [beers, setBeers] = useState([])
+    const [loading, setLoading] = useState(false)
+    
+    
+    async function getBeers() {
+        setLoading(true)
+        
+        const { data } = await axios.get('https://api.punkapi.com/v2/beers', {
+            params: {
+                per_page: 3,
+                page
+            }
+        });
 
+        console.log({page});
+        
+        setLoading(false);
+        setBeers(current => [...current, ...data]);
+        setPage(prev=>prev +  1);
+    }
+    
+    useEffect(() => {
+        getBeers();
+        console.log({beers});
+
+        //remove all beers when unmounted
+        return ()=>{
+            setBeers([]);
+            setPage(1);
+        };
+    }, [])
+    
     return (
-        <div>
-            This is home page.
-        </div>
+        <section >
+            {!!beers.length && (
+                <div>
+                    {beers.map((item_data, index) => (
+                        <Item item_data = {item_data}/>
+                    ))}
+                </div>
+            )}
+
+            <Button>Load More</Button>
+            
+            {loading && (
+                <>Loading...</>
+            )}
+        </section>
     );
 }
 
