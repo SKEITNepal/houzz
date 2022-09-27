01e68d5e8d27e0d605dce66abb35e29d4e1081fe
suraj.neupane <suraj.neupane151@gmail.com>
2022-09-27 13:40:44 +0545

    refactor: create root already decleared on index.js - removed from App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import Routes from "./Routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-
-const root = createRoot(container); 
-root.render(
-  <React.StrictMode>
+function App() {
+  return (
     <Routes />
-  </React.StrictMode>
-);
+  );
+}
 
-export default root;
+export default App;
